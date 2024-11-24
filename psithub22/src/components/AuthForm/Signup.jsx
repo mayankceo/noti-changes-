75069048cb51c3d90b81d40af947e406837bc30c
mayankceo <edufutura.wd@gmail.com>
2024-11-24 16:05:28 +0530

    Signup.jsx

--- psithub22/src/components/AuthForm/Signup.jsx
+++ psithub22/src/components/AuthForm/Signup.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { auth } from "./firebase";
+import { auth } from "../../firebase/firebase";
 
 const Signup = () => {
   const [email, setEmail] = useState("");
